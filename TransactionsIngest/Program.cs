cd2bb1d793a1005822fe2986b59c1b40efbc9fb1
yuva1231 <yuvasiddharth1231@gmail.com>
2026-03-25 13:04:57 -0500

    finish program.cs

--- TransactionsIngest/Program.cs
+++ TransactionsIngest/Program.cs
@@ -5,3 +5,26 @@ var config = new ConfigurationBuilder()
     .Build();
 
 var services = new ServiceCollection();
+
+services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(config.GetConnectionString("DefaultConnection")));
+
+services.AddSingleton<IConfiguration>(config);
+
+// perhaps can put in real implementation here for live API
+if (config.GetValue<bool>("AppSettings:UseMockFeed"))
+    services.AddTransient<ITransactionApiService, MockTransactionApiService>();
+else
+    services.AddTransient<ITransactionApiService, MockTransactionApiService>();
+
+services.AddTransient<IngestionService>();
+
+var provider = services.BuildServiceProvider();
+
+// create the database tables if they don't exist yet
+var db = provider.GetRequiredService<AppDbContext>();
+db.Database.EnsureCreated();
+
+// run the ingestion job
+var ingestion = provider.GetRequiredService<IngestionService>();
+await ingestion.RunAsync();
