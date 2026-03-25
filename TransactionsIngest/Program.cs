8bd4bc70eb326e78f3ada1840c44afa4f2543b27
yuva1231 <yuvasiddharth1231@gmail.com>
2026-03-25 12:01:04 -0500

    start working on program.cs

--- TransactionsIngest/Program.cs
+++ TransactionsIngest/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+
+var services = new ServiceCollection();
